2e63401e7020d0844cf9037c34172c0dd46a466e
PMT <pmt9819@gmail.com>
2024-08-22 17:07:25 +0700

    Update project

--- pages/index.js
+++ pages/index.js
@@ -12,53 +12,55 @@ export default function Home() {
         <meta name="description" content="Yes Chad embodies internet culture's power to transform divisive elements into universal symbols of coolness and humor, evolving humanity's collective psyche one 'yes' at a time." />
       </Head>
       <div className={`${font.className} bg-[#67D9FD] text-lg`}>
-        <div className="max-w-7xl mx-auto mb-16 md:mb-24 px-4">
-          <div className="mb-4">
-            <Image
-              src="/chad.gif"
-              width={445}
-              height={480}
-              className="w-36 h-auto block mx-auto"
-              alt=""
-              unoptimized
-            />
-          </div>
-          <div className="flex justify-center gap-4 font-semibold flex-wrap mb-4">
-            <a
-              href="https://t.me/solyeschad"
-              className="flex items-center bg-white px-4 py-2 rounded-md outline outline-2 shadow-solid-primary shadow-black"
-              target="_blank"
-              title="Telegram"
-            >
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-5"
+        <div className="bg-[#2B8CAD]">
+          <div className="max-w-7xl mx-auto mb-16 md:mb-24 px-4 py-2 flex items-center justify-between">
+            <div className="mb-4">
+              <Image
+                src="/chad.gif"
+                width={445}
+                height={480}
+                className="w-24 md:w-32 h-auto block mx-auto"
+                alt=""
+                unoptimized
+              />
+            </div>
+            <div className="flex justify-center gap-4 font-semibold flex-wrap mb-4">
+              <a
+                href="https://t.me/solyeschad"
+                className="flex items-center bg-amber-100 px-4 py-2 rounded-md outline outline-2 shadow-solid-primary shadow-black"
+                target="_blank"
+                title="Telegram"
               >
-                <path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z" />
-              </svg>
-            </a>
-            <a
-              href="https://x.com/solyeschad"
-              className="flex items-center bg-white px-4 py-2 rounded-md outline outline-2 shadow-solid-primary shadow-black"
-              target="_blank"
-              title="X"
-            >
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-5"
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5"
+                >
+                  <path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z" />
+                </svg>
+              </a>
+              <a
+                href="https://x.com/solyeschad"
+                className="flex items-center bg-amber-100 px-4 py-2 rounded-md outline outline-2 shadow-solid-primary shadow-black"
+                target="_blank"
+                title="X"
               >
-                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-              </svg>
-            </a>
-            <a
-              href="https://jup.ag/swap/SOL-7rdeLkyfmxujFthUNYZM7jWGEKZnT9mkeSGG1c9hpump"
-              className="flex items-center bg-white px-4 py-2 rounded-md outline outline-2 shadow-solid-primary shadow-black"
-              target="_blank"
-            >
-              BUY $CHAD
-            </a>
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5"
+                >
+                  <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+                </svg>
+              </a>
+              <a
+                href="https://jup.ag/swap/SOL-7rdeLkyfmxujFthUNYZM7jWGEKZnT9mkeSGG1c9hpump"
+                className="flex items-center bg-amber-100 px-4 py-2 rounded-md outline outline-2 shadow-solid-primary shadow-black"
+                target="_blank"
+              >
+                BUY $CHAD
+              </a>
+            </div>
           </div>
         </div>
         <div className="max-w-7xl mx-auto mb-16 md:mb-24 px-4">
@@ -273,8 +275,10 @@ export default function Home() {
             <iframe src="https://dexscreener.com/solana/7rdeLkyfmxujFthUNYZM7jWGEKZnT9mkeSGG1c9hpump?embed=1"></iframe>
           </div>
         </div>
-        <div className="max-w-7xl mx-auto px-4 py-4 text-center font-semibold">
-          Copyright © 2024 Yes Chad
+        <div className="bg-[#2B8CAD]">
+          <div className="max-w-7xl mx-auto px-4 py-4 text-center font-semibold">
+            Copyright © 2024 Yes Chad
+          </div>
         </div>
       </div>
     </>
