9b08825a9ee87680ef8cea5fc6dd125d0ad7bc98
PMT <pmt9819@gmail.com>
2024-08-24 20:58:53 +0700

    Update project

--- components/ChadImage.js
+++ components/ChadImage.js
@@ -1,23 +1,26 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import Image from 'next/image'
 
 export default function Chad() {
-  const images = [
-    '/Chad/0.png',
-    '/Chad/1.png',
-    '/Chad/2.png',
-    '/Chad/3.png',
-    '/Chad/4.png',
-    '/Chad/5.png',
-    '/Chad/6.png',
-    '/Chad/7.png',
-    '/Chad/8.png',
-    '/Chad/9.png',
-    '/Chad/10.png',
-    '/Chad/11.png',
-    '/Chad/12.png',
-    '/Chad/13.png',
-  ]
+  const images = useMemo(
+    () => [
+      '/Chad/0.png',
+      '/Chad/1.png',
+      '/Chad/2.png',
+      '/Chad/3.png',
+      '/Chad/4.png',
+      '/Chad/5.png',
+      '/Chad/6.png',
+      '/Chad/7.png',
+      '/Chad/8.png',
+      '/Chad/9.png',
+      '/Chad/10.png',
+      '/Chad/11.png',
+      '/Chad/12.png',
+      '/Chad/13.png',
+    ],
+    []
+  )
   const [index, setIndex] = useState(0)
   const handleNext = () => {
     setIndex((prevIndex) => (prevIndex + 1) % images.length)
@@ -28,7 +31,16 @@ export default function Chad() {
 
   return (
     <div>
-      <Image src={images[index]} width={360} height={360} alt={`Chad ${index + 1}`} className="block w-72 h-auto mx-auto" />
+      {images.map((image, i) => (
+        <Image
+          key={i}
+          src={image}
+          width={360}
+          height={360}
+          alt={`Chad ${index + 1}`}
+          className={`w-72 h-auto mx-auto ${i == index ? 'block' : 'hidden'}`}
+        />
+      ))}
       <div className="flex justify-center space-x-6">
         <button onClick={handlePrev} className="bg-amber-100 hover:bg-white p-1 rounded-md outline outline-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
