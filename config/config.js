828a5ba7067302a08022f203b79667145149157f
PMT <pmt9819@gmail.com>
2024-08-24 17:43:59 +0700

    Update project

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// config.js
+const config = {
+  title: '$chad - Yes Chad Token On Solana',
+  description: 'Yes Chad embodies internet culture\'s power to transform divisive elements into universal symbols of coolness and humor, evolving humanity\'s collective psyche one "yes" at a time.',
+  url: 'https://solyeschad.com',
+  ogImage: '',
+  googleSiteVerification: '_OnLsmpDxrDhS4ZYp4Z7R3qSR_fgvx8YWN24BghGJdo',
+}
+
+export default config
