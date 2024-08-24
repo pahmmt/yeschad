0600875272fb454cc1f1c38084e15fd9d1de1007
PMT <pmt9819@gmail.com>
2024-08-24 17:45:36 +0700

    Update project

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Oswald } from 'next/font/google'
 import config from '../config/config'
-
 import ChadImage from '../components/ChadImage'
 
 const font = Oswald({ subsets: ['latin'] })
