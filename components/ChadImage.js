8a1c2b5ba46e61627e6e272d9efc5c54aa881a7a
PMT <pmt9819@gmail.com>
2024-08-24 17:44:27 +0700

    Update project

--- components/ChadImage.js
+++ components/ChadImage.js
@@ -28,45 +28,16 @@ export default function Chad() {
 
   return (
     <div>
-      <Image
-        src={images[index]}
-        width={360}
-        height={360}
-        alt={`Image ${index + 1}`}
-        className="block w-72 h-auto mx-auto"
-        priority="low"
-      />
+      <Image src={images[index]} width={360} height={360} alt={`Image ${index + 1}`} className="block w-72 h-auto mx-auto" priority="low" />
       <div className="flex justify-center space-x-6">
         <button onClick={handlePrev} className="bg-amber-100 hover:bg-white p-1 rounded-md outline outline-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5 8.25 12l7.5-7.5"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
           </svg>
         </button>
         <button onClick={handleNext} className="bg-amber-100 hover:bg-white p-1 rounded-md outline outline-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-5"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m8.25 4.5 7.5 7.5-7.5 7.5"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
           </svg>
         </button>
       </div>
